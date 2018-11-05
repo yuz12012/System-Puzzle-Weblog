086a25cf24a63414e1aae80743537fdc137ea9d8
yuz12012 <yue.2.zhao@uconn.edu>
2018-11-05 14:35:13 -0500

    db fix

--- docker-entrypoint-initdb.d/init-tables.sh
+++ docker-entrypoint-initdb.d/init-tables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
-
-psql -v ON_ERROR_STOP=1 --username dbusr --dbname web <<-EOSQL
+echo "It is running!"
+psql -v ON_ERROR_STOP=1 --username dbuser --dbname webserver <<-EOSQL
         CREATE TABLE  weblogs (
                day    date,
                status varchar(3)
